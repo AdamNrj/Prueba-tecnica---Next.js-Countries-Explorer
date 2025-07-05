c0a067235312bb0ac413b85e36f974050117ca49
Manuel Naranjo <manuel.nrj.09@gmail.com>
2025-07-05 22:37:57 +0200

    fix: translated route corrections

--- app/[locale]/paises/[id]/page.tsx
+++ app/[locale]/paises/[id]/page.tsx
@@ -1,9 +1,5 @@
 import CountryPage from "@/app/pages/countries/country";
 
-interface PageProps {
-  params: { id: string };
-}
-
-export default function Page({ params }: PageProps) {
+export default function Page({ params }: { params: { id: string } }) {
   return <CountryPage params={params} />;
 }
--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,9 +1,16 @@
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  // A list of all locales that are supported
   locales: ["en", "es"],
-
-  // Used when no locale matches
   defaultLocale: "en",
+  pathnames: {
+    "/countries": {
+      en: "/countries",
+      es: "/paises",
+    },
+    "/countries/[id]": {
+      en: "/countries/[id]",
+      es: "/paises/[id]",
+    },
+  },
 });
