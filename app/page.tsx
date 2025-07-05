1439757c72767988f9170b1695ac9085a00ad1e8
Manuel Naranjo <manuel.nrj.09@gmail.com>
2025-07-05 19:13:45 +0200

    feat: multi-language added

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-3xl">
-      ¡Tailwind CSS funcionando!
-    </div>
-  );
-}
